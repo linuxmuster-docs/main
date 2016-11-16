6562630c64321ff0fd234c5cc63768e63f432799
T. Küchel <devel@zukuul.de>
2016-11-16 11:28:54 +0100

    Update conf.py
    try using the theme explicitely

--- source/conf.py
+++ source/conf.py
@@ -130,12 +130,13 @@ html_context = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # html_theme = 'linuxmuster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'theme_logo_only': True
+    'logo_only': True
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
