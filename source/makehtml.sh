ffc912ce00c8ac08ae48d8ebd97cfa791fc98d59
MachtDochNix <openlml@gollumundseinschatz.de>
2022-05-10 09:44:27 +0200

    Repetition of warning and error information at the end of the output from makehtml-script

--- source/makehtml.sh
+++ source/makehtml.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 cd ..
-make clean && make html 2> log.txt && echo -e "\nGefundene Fehler:\n" && cat log.txt && rm log.txt
+make clean && make html 2> log.txt
+if [ -s log.txt ]
+then
+	echo -e "\nGefundene Fehler:\n"
+        cat log.txt
+else
+	echo -e "\nGefundene Fehler: 0\n"
+fi
+rm log.txt
 cd -
