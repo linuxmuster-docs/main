e392d9c2078a55dd9cfd311371a73f2d5c9cb9f0
MachtDochNix <openlml@gollumundseinschatz.de>
2022-05-16 11:23:51 +0200

    Output of the script reordered for better readability.

--- source/makehtml.sh
+++ source/makehtml.sh
@@ -4,11 +4,11 @@ cd ..
 make clean && make html 2> log.txt
 if [ -s log.txt ]
 then
-	echo -e "\n$(tput setaf 1)Gefundene Fehler:$(tput setaf 7)\n"
         cat log.txt
+	echo -e "\n$(tput setaf 1)Gefundene Fehler:$(tput setaf 7)\n"
 else
-	echo -e "\n$(tput setaf 2)Gefundene Fehler: 0\n$(tput setaf 7)"
 	git status
+	echo -e "\n$(tput setaf 2)Gefundene Fehler: 0\n$(tput setaf 7)"
 fi
 rm log.txt
 cd -
